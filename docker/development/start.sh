2656bdf7335a54be8fc51c319b438b3e74926c2a
imrobsol <imrobsol@proton.me>
2023-11-19 21:18:25 +0700

    Ensure admin creation

--- docker/development/start.sh
+++ docker/development/start.sh
@@ -2,6 +2,6 @@
 
 # python manage.py compilemessages -v 0
 python manage.py migrate -v 0 --no-input
-# python manage.py ensure_admin --email admin@admin.com --password admin
+python manage.py ensure_admin --email admin@admin.com --password admin
 
 exec python manage.py runserver 0.0.0.0:8000
